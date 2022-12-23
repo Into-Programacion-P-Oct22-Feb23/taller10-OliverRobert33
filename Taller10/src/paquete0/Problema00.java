cc93cf8c24c8339328f53fba1e9b3da9c9647004
Oliver Saraguro <jspenarreta@utpl.edu.ec>
2022-12-23 11:17:12 -0500

    subiendo taller

--- Taller10/src/paquete0/Problema00.java
+++ Taller10/src/paquete0/Problema00.java
@@ -10,35 +10,37 @@ package paquete0;
  * @author reroes
  */
 public class Problema00 {
+
     public static void main(String[] args) {
-        double[][] notas = {{9, 10, 7, 8}, 
-                            {10, 5, 7, 9}, 
-                            {7, 9.4, 10, 9}};
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
         String[] nombresEstudiante = {"Jose A", "Elena J", "Paúl L"};
         double[] sumaNotas = new double[3];
-                            
-        double suma = 0;
+
+        double suma;
         String reporte = "";
-        
-        for (int i = 0; i < notas.length; i++) { 
-            
-            for (int j = 0; j < notas[i].length; j++) { 
+
+        for (int i = 0; i < notas.length; i++) {
+                suma = 0;
+            for (int j = 0; j < notas[i].length; j++) {
+                
                 suma = suma + notas[i][j];
-                     
+
             }
             sumaNotas[i] = suma; // 
         }
-        
+
         // Presentación de resultados
         for (int i = 0; i < nombresEstudiante.length; i++) {
             reporte = String.format("%sEstudiante %s tiene una sumataria "
-                    + "de notas: %.3f\n",
+                    + "de notas: %.2f\n",
                     reporte,
                     nombresEstudiante[i],
                     sumaNotas[i]);
         }
-        
+
         System.out.printf("%s\n", reporte);
-        
+
     }
 }
--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -5,10 +5,35 @@
  */
 package paquete2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema02 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double[][] datos = new double[2][2];
+        System.out.println("Ingrese datos decimales");
+        
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("Ingrese[" + i + "]" + "[" + j + "]=");
+                datos[i][j] = entrada.nextDouble();
+
+            }
+        }
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.println("El numero [" + i + "] es: " + datos[i][j]);
+
+            }
+        }
+    }
 }
