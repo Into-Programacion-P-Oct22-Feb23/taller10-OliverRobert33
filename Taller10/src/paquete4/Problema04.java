db61c9a3f334fe035a6c186da3f8d9b6d3ac3b32
Oliver Saraguro <rosaraguro@utpl.edu.ec>
2022-12-23 11:22:25 -0500

    Subiendo

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -4,11 +4,33 @@
  * and open the template in the editor.
  */
 package paquete4;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
 public class Problema04 {
     
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double[][] datos = new double[2][2];
+        System.out.println("Ingrese datos decimales");
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("Ingrese[" + i + "]" + "[" + j + "]= ");
+                datos[i][j] = entrada.nextDouble();
+
+            }
+        }
+
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.println("El numero [" + i + "] es: " + datos[i][j]);
+
+            }
+        }
+    }
 }
